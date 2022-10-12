8a83f03b20f0dc910a599189212e994b71bc6603
Jackalob <nike9712@gmail.com>
2022-10-13 00:06:13 +0800

    add router & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
