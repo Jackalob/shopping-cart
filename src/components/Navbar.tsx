0d397d44f03f3710227996af9f865dc92d0bef5e
Jackalob <nike9712@gmail.com>
2022-10-15 16:29:38 +0800

    add shopping cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,13 @@
-import { Container, Navbar as NavbarBS, Nav } from "react-bootstrap";
+import { Container, Navbar as NavbarBS, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-
-const x = <div />;
+import { ReactComponent as IonCart } from "../assets/icons/cart-shopping.svg";
 
 const Navbar = () => {
   return (
-    <NavbarBS className="bg-white shadow-sm mb-3">
+    <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav>
-          <Nav.Link to="/" as={NavLink} active>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
             Home
           </Nav.Link>
           <Nav.Link to="/store" as={NavLink}>
@@ -18,6 +17,27 @@ const Navbar = () => {
             About
           </Nav.Link>
         </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <IonCart style={{ fill: "currentColor" }} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
       </Container>
     </NavbarBS>
   );
