52bb8d34c36d6407e40e1ef76afb97407d904789
Jackalob <nike9712@gmail.com>
2022-10-19 09:30:32 +0800

    add total price of cart items

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utils/format";
 import CartItem from "./CartItem";
+import storeItems from "../data/items.json";
 
 type IProps = {
   isOpen: boolean;
@@ -8,14 +10,24 @@ type IProps = {
 
 const ShoppingCart = ({ isOpen }: IProps) => {
   const { cartItems, closeCart } = useShoppingCart();
+  const totalPrice = cartItems.reduce((total, carItem) => {
+    const item = storeItems.find((storeItem) => storeItem.id === carItem.id);
+    return total + (item?.price ?? 0) * carItem.quantity;
+  }, 0);
+
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>Cart</Offcanvas.Header>
       <Offcanvas.Body>
-        <Stack gap={5}>
-          {cartItems.map((carItem) => (
-            <CartItem key={carItem.id} {...carItem} />
-          ))}
+        <Stack className="h-100">
+          <Stack gap={5} className="h-100 overflow-auto mb-3">
+            {cartItems.map((carItem) => (
+              <CartItem key={carItem.id} {...carItem} />
+            ))}
+          </Stack>
+          <div className="ms-auto fw-bold fs-4">
+            Total {formatCurrency(totalPrice)}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
